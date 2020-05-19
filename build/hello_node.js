315126b6cd73102d1abb988f6790a4330941aab3
liam <liam@liam.local>
2020-05-19 10:17:15 +0800

    init;

--- build/hello_node.js
+++ build/hello_node.js
@@ -4,4 +4,5 @@ var http = require("http");
 var server = http.createServer(function (req, res) {
     res.end('hello node ?');
 });
+console.log('启动成功,并运行在8080端口');
 server.listen(8080);
--- server/hello_node.ts
+++ server/hello_node.ts
@@ -4,4 +4,5 @@ const server = http.createServer((req,res) => {
     res.end('hello node ?');
 });
 
+console.log('启动成功,并运行在8080端口')
 server.listen(8080);
